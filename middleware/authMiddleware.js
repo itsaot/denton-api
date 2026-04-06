ea2bc4eeb0c1d88c3893dd032b3e88eaec1aacc8
Jason <55947509+Jasonndlovu@users.noreply.github.com>
2026-04-06 09:50:45 +0200

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -21,4 +21,16 @@ const protect = async (req, res, next) => {
   }
 };
 
-module.exports = { protect };
+const restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        status: 'fail',
+        message: 'You do not have permission to perform this action',
+      });
+    }
+    next();
+  };
+};
+
+module.exports = { protect, restrictTo };
